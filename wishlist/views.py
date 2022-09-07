98cb4c21b22101884f164c5a4fe8b572430371a0
Rafi Ghalibin Abrar <rafighalibin@gmail.com>
2022-09-07 17:45:35 +0700

    Add name

--- wishlist/views.py
+++ wishlist/views.py
@@ -5,7 +5,7 @@ def show_wishlist(request):
     data_barang_wishlist = BarangWishlist.objects.all()
     context = {
         'list_barang': data_barang_wishlist,
-        'nama': ''
+        'nama': 'Rafi Ghalibin Abrar'
     }
     
     return render(request, "wishlist.html", context)
